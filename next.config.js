aa499d373f44f1ce097ad2cc6f55d24dfa314892
Roncyboy <tpaiement@my.bcit.ca>
2022-05-26 15:18:57 -0700

    test4

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: false,
+// }
 
-module.exports = nextConfig {
+module.exports = {
   eslint: {
   // Warning: This allows production builds to successfully complete even if
   // your project has ESLint errors.
